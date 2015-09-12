81fa63ffa6e5bbf668fa532cab6966b43a6976c7
jordan-bramble <jordan.bramble@opower.com>
2015-09-12 18:07:39 -0400

    alter article urls to pull from publications conservative, liberal, liberatarian

--- get_articles.py
+++ get_articles.py
@@ -1,17 +1,17 @@
-import BeautifulSoup as bs
 from requests import get
+from bs4 import BeautifulSoup as bs
 import newspaper as ns
 from nltk.corpus import brown, reuters
 import os
 import sys
 
-print reuters.categories()
-print brown.categories()
+#print reuters.categories()
+#print brown.categories()
 
 #construct builds of all segments on CNN
 #save to folders
 #build Trainer
-urls =['http://cnn.com/US','http://cnn.com/WORLD','http://cnn.com/POLITICS','http://cnn.com/JUSTICE','http://cnn.com/SHOWBIZ','http://cnn.com/TECH']
+urls =['http://www.cato.org/commentary']
 #cnn_paper = ns.build('http://cnn.com')
 #medium = ns.build('http://medium.com')
 
@@ -33,10 +33,10 @@ for url in urls:
             article.download()
             article.parse()
             print article.text
-            print url.split('/')[-1] + '/' + article.title.split(' ')[0] +'.txt'
-            path ='news/' + url.split('/')[-1] + '/' + article.title.split(' ')[0] +'.txt'
-            print os.path.dirname(url.split('/')[-1])
-            ensure_dir('news'+url.split('/')[-1])
+            print url.split('.')[1] + '/' + article.title.split(' ')[0] +'.txt'
+            path ='news/' + url.split('.')[1] + '/' + article.title.split(' ')[0] +'.txt'
+            print os.path.dirname(url.split('.')[1])
+            ensure_dir('news/'+url.split('.')[1])
             f = open(path,'wb')
             f.write(unicode(article.text).encode("utf-8"))
             f.close()
