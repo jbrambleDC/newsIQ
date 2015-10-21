26b9eff6d3b206f440a998552f59d5ac42019f3a
Jordan Bramble <jordan.bramble@opower.com>
2015-10-20 22:02:42 -0400

    allow get_articles to take a list of articles

--- get_articles.py
+++ get_articles.py
@@ -4,11 +4,17 @@ import newspaper as ns
 from nltk.corpus import brown, reuters
 import os
 import sys
+import re
 
 #construct builds of all segments on CNN
 #save to folders
 #build Trainer
-urls =['http://www.thedailybell.com']
+urls =[]
+for i in sys.argv:
+  match = re.search("(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)",i)
+  if match:
+    urls.append(i)
+print urls
 #urls = [url for url in cnn_paper.category_urls()]
 def ensure_dir(f):
     d = os.path.dirname(f)
@@ -28,9 +34,9 @@ for url in urls:
             article.parse()
             print article.text
             print url.split('.')[1] + '/' + article.title.replace(' ','_') +'.txt'
-            path ='validate_data/' + url.split('.')[1] + '/' + article.title.replace(' ','_').replace(':','').replace("'",'').replace('/','').replace(',','').replace('-','_').replace('.','').replace(';','') +'.txt'
+            path ='test_data/' + url.split('.')[1] + '/' + article.title.replace(' ','_').replace(':','').replace("'",'').replace('/','').replace(',','').replace('-','_').replace('.','').replace(';','') +'.txt'
             print os.path.dirname(url.split('.')[1])
-            ensure_dir('validate_data/'+url.split('.')[1])
+            ensure_dir('test_data/'+url.split('.')[1])
             f = open(path,'wb')
             f.write(unicode(article.text).encode("utf-8"))
             f.close()
