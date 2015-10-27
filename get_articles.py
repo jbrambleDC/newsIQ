80afb65b3b8a6ecbc4c902279d4627329f48c827
Jordan Bramble <jordan.bramble@opower.com>
2015-10-26 23:54:47 -0400

    Add plotly functionality for plotting frequency distributions

--- get_articles.py
+++ get_articles.py
@@ -1,21 +1,15 @@
 from requests import get
-from bs4 import BeautifulSoup as bs
 import newspaper as ns
-from nltk.corpus import brown, reuters
 import os
 import sys
 import re
 
-#construct builds of all segments on CNN
-#save to folders
-#build Trainer
 urls =[]
 for i in sys.argv:
   match = re.search("(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)",i)
   if match:
     urls.append(i)
 print urls
-#urls = [url for url in cnn_paper.category_urls()]
 def ensure_dir(f):
     d = os.path.dirname(f)
     if not os.path.exists(f):
