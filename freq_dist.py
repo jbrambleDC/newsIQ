e62900a8bf70874846b27bdabbb480592afda5cc
Jordan Bramble <jordan.bramble@opower.com>
2015-10-31 01:32:00 -0400

    add logging

--- freq_dist.py
+++ freq_dist.py
@@ -10,30 +10,36 @@ import plotly.graph_objs as go
 import sys
 import re
 
+import logging
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 ##allow to take a directory
 def get_files():
-    files =[]
-    for i in sys.argv:
-        match = re.match(".*\.txt$", i)
-        if match:
-            files.append(i)
-    return files
+  files =[]
+  for i in sys.argv:
+    match = re.match(".*\.txt$", i)
+    if match:
+      files.append(i)
+  logging.info("found %s files to parse", len(files))
+  return files
 
 def freq_dist(files):
-    pstm = PorterStemmer()
-    words = []
+  pstm = PorterStemmer()
+  words = []
+  if files.count > 0:
     for f in files:
-        with open(f,"rb") as curr_file:
-            data = word_tokenize(curr_file.read().replace('\n', ''))
-            words += [pstm.stem(d) for d in data if d.lower() not in stopwords.words('english') and len(d) > 5]
+      with open(f,"rb") as curr_file:
+        data = word_tokenize(curr_file.read().replace('\n', ''))
+        words += [pstm.stem(d) for d in data if d.lower() not in stopwords.words('english') and len(d) > 5]
     return dict(FreqDist(w.encode('utf-8').lower() for w in words).items()[0:100])
+  else:
+    logging.error("no files to process")
 
 def main():
-    terms = freq_dist(get_files())
-    data = [go.Bar(x=terms.keys(),y=terms.values())]
-    plot_url = ply.plot(data, filename='word_hist_lib')
+  terms = freq_dist(get_files())
+  data = [go.Bar(x=terms.keys(),y=terms.values())]
+  plot_url = ply.plot(data, filename='word_hist_lib')
 
 main()
--- get_articles.py
+++ get_articles.py
@@ -3,32 +3,40 @@ import newspaper as ns
 import os
 import sys
 import re
+import logging
 
-urls =[]
-for i in sys.argv:
-  match = re.search("(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)",i)
-  if match:
-    urls.append(i)
-print urls
+def get_urls():
+  urls =[]
+  for i in sys.argv:
+    match = re.search("(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)",i)
+    if match:
+      urls.append(i)
+  return urls
+
 def ensure_dir(f):
-    d = os.path.dirname(f)
-    if not os.path.exists(f):
-        os.makedirs(f)
+    #d = os.path.dirname(f)
+  if not os.path.exists(f):
+    os.makedirs(f)
 
-for url in urls:
-    print url
+def main(urls):
+  for url in urls:
+    logging.info('fetching from %s', url)
     paper = ns.build(url)
-    print len(paper.articles)
+    logging.info('sourcing %s articles', len(paper.articles))
+
     if len(paper.articles) >= 1:
-        for article in paper.articles:
-            article.download()
-            article.parse()
-            print article.text
-            print url.split('.')[1] + '/' + article.title.replace(' ','_') +'.txt'
-            path ='news/' + url.split('.')[1] + '/' + article.title.replace(' ','_').replace(':','').replace("'",'').replace('/','').replace(',','').replace('-','_').replace('.','').replace(';','') +'.txt'
-            print os.path.dirname(url.split('.')[1])
-            ensure_dir('news/'+url.split('.')[1])
-            f = open(path,'wb')
-            f.write(unicode(article.text).encode("utf-8"))
-            f.close()
+      for article in paper.articles:
+        article.download()
+        article.parse()
+        path ='news/' + url.split('.')[1] + '/' + article.title.replace(' ','_').replace(':','').replace("'",'').replace('/','').replace(',','').replace('-','_').replace('.','').replace(';','') +'.txt'
+        ensure_dir('news/'+url.split('.')[1])
+        logging.info('writing %s', path)
+        f = open(path,'wb')
+        f.write(unicode(article.text).encode("utf-8"))
+        f.close()
+
+    else:
+      logging.warning('paper sourced from %s is empty', url)
+
+main(get_urls())
 
